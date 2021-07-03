3ed03d7be764d0ea2d3f54adffa8beafc6bbccbc
Gabrijel <gabrijel.golubic@kiwi.com>
2021-07-03 15:07:37 +0200

    rename fe2 to fe

---
+++ frontend/src/components/scenes/Auth/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import 'antd/dist/antd.css';
+import { AuthContainer, BackgroundContainer, RouteContainer, RouteWrapper } from './Auth.styled';
+
+import Login from './scenes/Login';
+
+const Content = () => {
+  return (
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      {/* <Route path='/register' component={Register} />
+      <Route path='/activate' component={Landing} />
+      <Route path='/reset-password' component={ResetPassword} /> */}
+    </Switch>
+  );
+};
+
+const Auth = () => {
+  return (
+    <AuthContainer>
+      <BackgroundContainer></BackgroundContainer>
+      <RouteContainer>
+        <RouteWrapper>
+          <Content />
+        </RouteWrapper>
+      </RouteContainer>
+    </AuthContainer>
+  );
+};
+
+export default Auth;
---
+++ frontend/src/components/scenes/Auth/scenes/Login/index.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin: 48px;
+  max-width: 400px;
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
