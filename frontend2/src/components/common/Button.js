6ef236d50762e1f55497c60be29bb480b3c505e1
Gabrijel <gabrijel.golubic@kiwi.com>
2021-07-04 10:19:09 +0200

    vscode diff issue

--- frontend2/src/components/common/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-import AntdButton from 'antd/lib/button';
-
-const Button = styled(props => <AntdButton {...props} />)`
-  font-weight: 600;
-`;
-
-export default Button;
