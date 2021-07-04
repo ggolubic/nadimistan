423192aca036722e1f19c9b84e94fe339c689bde
Gabrijel <gabrijel.golubic@kiwi.com>
2021-07-04 23:28:22 +0200

    add unsecure login flow

--- app/sql/main.py
+++ app/sql/main.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, FastAPI, HTTPException
+from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -17,7 +19,77 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
+fake_users_db = {
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+    "bob": {
+        "username": "bob",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+}
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def get_user(db, email: str):
+    if email in db:
+        user_dict = db[email]
+        return schemas.UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: schemas.User = Depends(get_current_user),
+):
+    if current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.email)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    user = schemas.UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    return {"access_token": user.email, "token_type": "bearer"}
+
+
+@app.post("/users", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
--- app/sql/schemas.py
+++ app/sql/schemas.py
@@ -30,6 +30,10 @@ class UserCreate(UserBase):
     password: str
 
 
+class UserInDB(UserBase):
+    hashed_password: str
+
+
 class User(UserBase):
     id: int
     is_active: bool
