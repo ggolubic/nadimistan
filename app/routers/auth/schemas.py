0fde4514a19c39307a79d31c19f1d10f22b5a2dc
Gabrijel <gabrijel.golubic@kiwi.com>
2021-07-19 21:18:20 +0200

    add BE session support, minor cleanup

--- app/routers/auth/schemas.py
+++ app/routers/auth/schemas.py
@@ -33,15 +33,3 @@ class AuthenticatedUser(User):
 
 class TokenData(BaseModel):
     email: Optional[str] = None
-
-
-def ResponseModel(data, message: Optional[str]):
-    return {
-        "data": [data],
-        "code": 200,
-        "message": message,
-    }
-
-
-def ErrorResponseModel(error, code, message):
-    return {"error": error, "code": code, "message": message}
