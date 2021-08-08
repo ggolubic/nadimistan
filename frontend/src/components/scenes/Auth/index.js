00d4cf44d30b7affe632387b60b1fb5f104e3a81
Gabrijel Golubić <gabrijel.golubic@gmail.com>
2021-08-08 11:34:31 +0200

    remove old code, merge landing with auth (#22)

--- frontend/src/components/scenes/Auth/index.js
+++ frontend/src/components/scenes/Auth/index.js
@@ -1,31 +1,84 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { AuthContainer, BackgroundContainer, RouteContainer, RouteWrapper } from './Auth.styled';
+import React, { useEffect, useLayoutEffect, useContext } from 'react';
+import { useHistory, Switch, Route } from 'react-router-dom';
+import Paragraph from 'antd/lib/typography/Paragraph';
 
-import Login from './scenes/Login';
+import { Title } from 'components/common/Typography';
+import Hero from 'components/common/Hero';
+import { AuthContext } from 'components/services/Auth/AuthProvider';
+import { getCookie } from 'utils/cookie';
+import { setAuthToken } from 'utils/api';
+import { ROUTE_CONFIG } from 'consts/routes';
+
+import { PageWrapper, NavBar, Banner, Brief } from './index.styled';
+import { Login, Register, Activation, ResetPassword } from './scenes';
 
 const Content = () => {
   return (
     <Switch>
-      <Route exact path="/login" component={Login} />
-      {/* <Route path='/register' component={Register} />
-      <Route path='/activate' component={Landing} />
-      <Route path='/reset-password' component={ResetPassword} /> */}
+      <Route exact path={ROUTE_CONFIG.AUTH.LOGIN} component={Login} />
+      <Route path={ROUTE_CONFIG.AUTH.REGISTER} component={Register} />
+      <Route path={ROUTE_CONFIG.AUTH.ACTIVATE} component={Activation} />
+      <Route path={ROUTE_CONFIG.AUTH.RESET_PASSWORD} component={ResetPassword} />
     </Switch>
   );
 };
 
-const Auth = () => {
+const Landing = props => {
+  const history = useHistory();
+  const authCtx = useContext(AuthContext);
+
+  useLayoutEffect(() => {
+    const token = getCookie('ath');
+
+    if (token) {
+      setAuthToken(token);
+      authCtx.getSession();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!authCtx.user) {
+      return;
+    }
+
+    const { from } = props.location.state || {
+      from: { pathname: props.history.location.pathname || '/' },
+    };
+
+    if (from.pathname === '/') {
+      history.push(`/search`);
+      return;
+    }
+
+    if (!authCtx.loggingIn && !authCtx.loginError && authCtx.user) {
+      history.push(from.pathname);
+    }
+  }, [authCtx.loggingIn]);
+
   return (
-    <AuthContainer>
-      <BackgroundContainer></BackgroundContainer>
-      <RouteContainer>
-        <RouteWrapper>
+    <PageWrapper>
+      <NavBar>
+        <Title fontSize={36} primaryColor>
+          NađiMiStan
+        </Title>
+      </NavBar>
+      <Hero>
+        <Banner>
           <Content />
-        </RouteWrapper>
-      </RouteContainer>
-    </AuthContainer>
+        </Banner>
+      </Hero>
+      <Brief>
+        <Title level={2} primaryColor>
+          Dosta Vam je svakodnevnog scrollanja po raznim oglasnicima u potrazi za stanom?
+        </Title>
+        <Paragraph>
+          <span>NađiMiStan</span> Vam omogućava da na jednom mjestu pronađete sve što Vam treba te
+          uključuje besplatnu pretplatu na kojoj postavite kriterije a mi Vas obavijestimo kada se
+          takav stan pojavi na oglasniku.
+        </Paragraph>
+      </Brief>
+    </PageWrapper>
   );
 };
 
-export default Auth;
+export default Landing;
---
+++ frontend/src/components/scenes/Auth/scenes/index.js
@@ -0,0 +1,4 @@
+export { default as Activation } from './Activation';
+export { default as Login } from './Login';
+export { default as ResetPassword } from './ResetPassword';
+export { default as Register } from './Register';
